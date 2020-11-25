6a3bdd13063fe4ee72c08b157864ec851d2f7c4c
Maurici Yagües <mauriciyagues@gmail.com>
2020-11-25 19:01:04 +0100

    Use PIL image resize instead of TF

--- datasets/load_blender.py
+++ datasets/load_blender.py
@@ -1,17 +1,14 @@
 import functools
-import imageio
 import json
 import os
 
+import imageio
 import jax
 
 import numpy as np
-import tensorflow as tf
 
 from absl import logging
-
-# make sure TF does not allocate gpu memory
-tf.config.experimental.set_visible_devices([], "GPU")
+from PIL import Image
 
 trans_t = lambda t: np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, t], [0, 0, 0, 1]])
 
@@ -58,26 +55,24 @@ def load_data(dataset_dir, half_res=True, testskip=1):
         )
         for frame in meta["frames"][::skip]:
             fname = os.path.join(dataset_dir, f"{frame['file_path']}.png")
-            imgs.append(imageio.imread(fname))  # keep all 4 channels (RGBA)
+            imgs.append(Image.open(fname))
             poses.append(np.array(frame["transform_matrix"]))
 
-        all_imgs.append(imgs)
-        all_poses.append(poses)
+        all_imgs.extend(imgs)
+        all_poses.extend(poses)
         counts.append(len(imgs))
 
-    all_imgs = (np.concatenate(all_imgs) / 255.0).astype(np.float32)
-    all_poses = np.concatenate(all_poses).astype(np.float32)
-
-    img_height, img_width = all_imgs[0].shape[:2]
+    img_height, img_width = imgs[0].size
     focal = 0.5 * img_width / np.tan(0.5 * float(meta["camera_angle_x"]))
 
     if half_res:
         img_height //= 2
         img_width //= 2
         focal /= 2.0
-        all_imgs = tf.image.resize(
-            all_imgs, [img_height, img_width], method=tf.image.ResizeMethod.AREA
-        )._numpy()
+        all_imgs = list(map(lambda x: x.resize((img_height, img_width)), all_imgs))
+
+    all_imgs = (np.stack(all_imgs) / 255.0).astype(np.float32)
+    all_poses = np.stack(all_poses).astype(np.float32)
 
     render_poses = map(
         lambda angle: pose_spherical(angle, -30.0, 4.0),
